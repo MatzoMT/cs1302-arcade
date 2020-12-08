e260261e8dde03a13eab9a3b7d858091e4461516
Matthew Tzou <mdt82321@uga.edu>
2020-12-07 19:01:11 -0500

    updated compile.sh to update ChessDriver class

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ mvn -q -e clean
 mvn -q -e compile
 
 export MAVEN_OPTS=-Dprism.order=sw;
-mvn -q -e exec:java -Dexec.mainClass="cs1302.arcade.ArcadeDriver"
+mvn -q -e exec:java -Dexec.mainClass="cs1302.arcade.ChessDriver"
